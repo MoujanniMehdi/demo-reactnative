a86460711471ac64752883a1cf1a679ae29276a5
MoujanniMehdi <moujannimehdi@gmail.com>
2021-03-04 16:53:57 +0100

    second commit

--- App.js
+++ App.js
@@ -1,57 +1,84 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import {
-  TouchableOpacity,
-  ImageBackground,
-  StyleSheet,
-  Text,
-  View,
-  Alert,
-  SafeAreaView,
-  Image,
-} from "react-native";
+import * as React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
 
-const image = {
-  uri: "http://cn8ce.e-monsite.com/medias/images/logo-off.jpg?fx=r_550_550",
-};
+const Tab = createMaterialBottomTabNavigator();
 
-const onPress = () => Alert.alert("Test");
-
-export default function App() {
+// Feed Tab
+function Feed() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Big Title ?</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Feed!</Text>
+    </View>
+  );
+} // End of Feed tab
 
-      <TouchableOpacity style={styles.button} onPress={onPress}>
-        <Text>Press to find out</Text>
-      </TouchableOpacity>
+//Profile tab
+function Profile() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Profile!</Text>
+    </View>
+  );
+} //End of Profile tab
 
-      <Image source={image} style={styles.backgroundImage} />
+// Notification tab
+function Notifications() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Notifications!</Text>
+    </View>
+  );
+} // End of Notification tab
 
-      <StatusBar style="auto" />
-    </SafeAreaView>
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Feed"
+      activeColor="#e7ab3c"
+      labelStyle={{ fontSize: 12 }}
+      style={{ backgroundColor: "tomato" }}
+    >
+      <Tab.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{
+          tabBarLabel: "Updates",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  text: {
-    textAlign: "center",
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
-    zIndex: 3000,
-  },
-
-  button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10,
-    marginTop: 155,
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
